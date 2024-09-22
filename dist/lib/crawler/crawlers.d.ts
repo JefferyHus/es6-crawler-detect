ecaae703df03485d83cfab3752bf231a245a87a1
jeffery kuchiki <jefferytutorials@gmail.com>
2024-09-22 11:15:58 +0100

    feat: include sourcemaps and .d.ts files

---
+++ dist/lib/crawler/crawlers.d.ts
@@ -0,0 +1,5 @@
+import { Provider } from './provider';
+export declare class Crawlers implements Provider {
+    getAll(): string[];
+}
+//# sourceMappingURL=crawlers.d.ts.map
---
+++ dist/lib/crawler/exclusions.d.ts
@@ -0,0 +1,5 @@
+import { Provider } from './provider';
+export declare class Exclusions implements Provider {
+    getAll(): string[];
+}
+//# sourceMappingURL=exclusions.d.ts.map
