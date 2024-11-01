1fb1cb45a0515cf6fed01bfc878e25b8bab205db
Jeffery ThaGintoki <jefferytutorials@gmail.com>
2024-11-01 07:17:00 +0000

    fix: remove the unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,6 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default [{
     ignores: [
